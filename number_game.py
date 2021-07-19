ae90c61edba3d39bf0a53df210ca18e629e0b3bc
sabalyshen <tedoldhead@gmail.com>
2021-07-19 22:45:04 +0800

    first

---
+++ number_game.py
@@ -0,0 +1,14 @@
+#這是數字遊戲
+import random
+r = random.randint(1, 500)
+while True:
+	a = input('please type a number :')
+	a = int(a)
+	if a > r:
+		print('less')
+	elif a < r:
+		print('more')
+	else:
+		print('your right!!Good job.')
+		break
+		
