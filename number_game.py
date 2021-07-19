0fe9117a36d57f2a4ec907b8264bd6b74e13710d
sabalyshen <tedoldhead@gmail.com>
2021-07-19 23:28:36 +0800

    add count

--- number_game.py
+++ number_game.py
@@ -1,9 +1,13 @@
 #這是數字遊戲
 import random
 r = random.randint(1, 500)
+count = 0
+count = int(count)
 while True:
 	a = input('please type a number :')
 	a = int(a)
+	count += 1 # 
+	print('這是第', count, '次')
 	if a > r:
 		print('less')
 	elif a < r:
