abab64e430b001aebe105681b3b56d5b69506f53
sabalyshen <tedoldhead@gmail.com>
2021-07-19 23:35:03 +0800

    add value

--- number_game.py
+++ number_game.py
@@ -1,6 +1,10 @@
 #這是數字遊戲
 import random
-r = random.randint(1, 500)
+b = input('請輸入最小值')
+c = input('請輸入最大值')
+b = int(b)
+c = int(c)
+r = random.randint(b, c)
 count = 0
 count = int(count)
 while True:
